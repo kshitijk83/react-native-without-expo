b18f6d6f948b8d10d343a428aeb503306ac691dc
kshitijk83 <kshitijk83@gmail.com>
2019-06-20 16:57:22 +0530

    swipe animation optimized

--- parac/src/components/blah/blah.js
+++ parac/src/components/blah/blah.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { View, Easing, Text, Button, DatePickerAndroid, Dimensions, Animated, PanResponder, StyleSheet } from 'react-native';
+import { View, Easing, Text, Image, Dimensions, Animated, PanResponder, StyleSheet } from 'react-native';
 import { centerStyle } from '../../commonStyles/styles';
+import Cross from '../../assets/cancel.png';
 
 class Blah extends Component {
     constructor(props) {
         super(props);
         this.animCircleTop = new Animated.ValueXY();
         this.animCircleBottom = new Animated.ValueXY();
-        this.winWidth = Dimensions.get('window').width;
         this.winHeight = Dimensions.get('window').height;
         this.direction;
         this.state = {
@@ -40,7 +40,6 @@ class Blah extends Component {
                     this.state.animCircleTop.setValue({ x: 0, y: gestureState.dy });
                 } else {
                     this.state.animCircleBottom.setValue({ x: 0, y: gestureState.dy });
-                    console.log(this.state.animCircleBottom.y);
                 }
             },
             onPanResponderRelease: (event, gestureState) => {
@@ -60,54 +59,117 @@ class Blah extends Component {
         })
     }
 
-    render() {
+    topCircleAnimation = (animationObj) => {
         let circleTopy;
-        let circleBottom;
-        circleTopy = this.state.animCircleTop.y
+        circleTopy = animationObj.y
             .interpolate({
                 inputRange: [-this.winHeight, -this.winHeight / 3, 0],
-                outputRange: [200, 200, 0]
+                outputRange: [this.winHeight / 2.4, this.winHeight / 2.4, 0]
             });
+        let circleScaleTop;
+        circleScaleTop = animationObj.y.interpolate({
+            inputRange: [-this.winHeight / 2, -this.winHeight / 3, 0],
+            outputRange: [2, 1.2, 0]
+        })
 
-        circleBottom = this.state.animCircleBottom.y
+        let rotateTopCircle;
+        rotateTopCircle = animationObj.y.interpolate({
+            inputRange: [-this.winHeight, -this.winHeight / 3, 0],
+            outputRange: ['360deg', '360deg', '0deg']
+        })
+
+        return { circleTopy, circleScaleTop, rotateTopCircle };
+    }
+
+    bottomCircleAnimation = (animationObj) => {
+        let circleBottom;
+        circleBottom = animationObj.y
             .interpolate({
-                inputRange: [0, this.winHeight / 3, this.winHeight,],
-                outputRange: [0, -200, -200]
+                inputRange: [0, this.winHeight / 3, this.winHeight],
+                outputRange: [0, -this.winHeight / 2.4, -this.winHeight / 2.4]
             });
+        let circleScaleBottom;
+        circleScaleBottom = animationObj.y.interpolate({
+            inputRange: [0, this.winHeight / 3, this.winHeight / 2],
+            outputRange: [0, 1.2, 2]
+        })
+        let rotateBottomCircle;
+        rotateBottomCircle = animationObj.y.interpolate({
+            inputRange: [0, this.winHeight / 3, this.winHeight / 2],
+            outputRange: ['0deg', '360deg', '360deg']
+        })
+
+        return { circleBottom, circleScaleBottom, rotateBottomCircle };
+    }
+
+    render() {
+
+        let { circleTopy, circleScaleTop, rotateTopCircle } = this.topCircleAnimation(this.state.animCircleTop);
+        let { circleBottom, circleScaleBottom, rotateBottomCircle } = this.bottomCircleAnimation(this.state.animCircleBottom)
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Animated.View style={[styles.circleTop, { translateY: circleTopy }]} />
-                <Animated.View style={[styles.card,]} {...this.pan.panHandlers} />
-                <Animated.View style={[styles.circleBottom, { translateY: circleBottom }]} />
+            <View style={styles.center}>
+                <View style={[styles.card]} {...this.pan.panHandlers} />
+                <Animated.Image style={[
+                    styles.circleStyles,
+                    styles.topCircle,
+                    {
+                        transform: [{
+                            translateY: circleTopy,
+                        }, {
+                            rotate: rotateTopCircle
+                        }, {
+                            scaleX: circleScaleTop,
+                        }, {
+                            scaleY: circleScaleTop,
+                        }]
+                    }]}
+                    source={require('../../assets/checked.png')} />
+                <Animated.Image
+                    style={[
+                        styles.circleStyles,
+                        styles.bottomCircle,
+                        {
+                            transform: [{
+                                translateY: circleBottom,
+                            }, {
+                                rotate: rotateBottomCircle
+                            }, {
+                                scaleX: circleScaleBottom,
+                            }, {
+                                scaleY: circleScaleBottom
+                            }]
+                        }]}
+                    source={require('../../assets/cancel.png')} />
             </View >
         )
     }
 }
 
+
 const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     card: {
-        width: 200,
-        height: 200,
-        backgroundColor: 'black',
-        zIndex: -5500
+        width: '80%',
+        height: 400,
+        backgroundColor: 'blue',
+        zIndex: -10
     },
-    circleTop: {
+    circleStyles: {
         width: 50,
         height: 50,
         borderRadius: 25,
-        backgroundColor: 'red',
         position: 'absolute',
-        top: 0,
-        zIndex: 20000
+        zIndex: 10
     },
-    circleBottom: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        backgroundColor: 'red',
-        position: 'absolute',
-        bottom: 0,
-        zIndex: 20000
+    topCircle: {
+        top: 0
+    },
+    bottomCircle: {
+        bottom: 0
     }
 })
 
